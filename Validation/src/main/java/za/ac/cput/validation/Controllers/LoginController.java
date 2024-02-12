f7b003d7626e469beed198a0dd1fd7da362b2460
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-02-12 16:40:29 +0200

    login validation complete

--- Validation/src/main/java/za/ac/cput/validation/Controllers/LoginController.java
+++ Validation/src/main/java/za/ac/cput/validation/Controllers/LoginController.java
@@ -1,7 +1,11 @@
 package za.ac.cput.validation.Controllers;
 
+//make sure to use this import instead of javax
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +23,13 @@ public class LoginController {
     }
 
     @PostMapping("/processLogin")
-    public String processLoginForm(Model model, LoginModel loginModel){
+    //the @Valid make sure that the model being passed has valid data, bindingResult holds the errors that we use to check
+    public String processLoginForm(@Valid LoginModel loginModel, BindingResult bindingResult, Model model){
+        System.out.println(bindingResult.hasErrors());
+        if(bindingResult.hasErrors()){
+            model.addAttribute("loginModel", loginModel);
+            return "LoginForm";
+        }
         model.addAttribute("loginModel", loginModel);
         System.out.println(loginModel);
         return "LoginResults";
--- Validation/src/main/java/za/ac/cput/validation/Models/LoginModel.java
+++ Validation/src/main/java/za/ac/cput/validation/Models/LoginModel.java
@@ -1,8 +1,17 @@
 package za.ac.cput.validation.Models;
 
+//make sure to use this import instead of javax
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class LoginModel {
 
+    // the below 2 annotations are for validation, self explain
+    @NotNull(message="Username is required")
+    @Size(min=3, max=15, message="Username needs to be between 3-15 characters")
     private String username;
+    @NotNull(message="Password is required")
+    @Size(min=8,max=20, message="Password should be between 8-20 characters")
     private String password;
 
     public LoginModel() {
